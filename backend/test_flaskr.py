157e76f9561198ae199612c72913c5382a58d2fe
AdamElhawary <mohamedadel.elhawary@yahoo.com>
2020-09-14 19:36:52 +0200

    fixed PEP8 second time

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,8 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}/{}".format(
+            'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,7 +25,7 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         print("Test Completed! \n")
@@ -32,16 +33,18 @@ class TriviaTestCase(unittest.TestCase):
 
     """
     TODO
-    Write at least one test for each test for successful operation and for expected errors.
+    Write at least one test for each test for successful operation and
+     for expected errors.
     """
-    #Get_categories Endpoint!
+    # Get_categories Endpoint!
+
     def test_get_categories(self):
         result = self.client().get('/categories')
         data = json.loads(result.data)
         self.assertEqual(result.status_code, 200)
         self.assertEqual(data['success'], True)
 
-    #Get_paginated Quz Endpoint "200"!
+    # Get_paginated Quz Endpoint "200"!
     def test_get_paginated_questions(self):
         result = self.client().get('/questions')
         data = json.loads(result.data)
@@ -49,16 +52,16 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertTrue(len(data['questions']))
 
-    #Get_paginated Quz Endpoint "404"!
+    # Get_paginated Quz Endpoint "404"!
     def test_paginated_questions_404(self):
         result = self.client().get('/questions?page=1234')
         data = json.loads(result.data)
         self.assertEqual(resultult.status_code, 404)
         self.assertEqual(data['success'], False)
 
-    #Delete_Quz "200"
+    # Delete_Quz "200"
     def test_delete_question_by_ID(self):
-        #Creates a ques to delete it later on!
+        # Creates a ques to delete it later on!
         new_question = Question(
             question=self.question['question'],
             answer=self.question['answer'],
@@ -69,23 +72,23 @@ class TriviaTestCase(unittest.TestCase):
         result = self.client().delete('/questions/{}'.format(new_question.id))
         data = json.loads(result.data)
         self.assertEqual(result.status_code, 200)
-        self.assertEqual(data['success'], True)  
+        self.assertEqual(data['success'], True)
 
-    #Delete a not found Quz "404"
+    # Delete a not found Quz "404"
     def test_delete_question_404(self):
         result = self.client().delete('/question/1234')
         data = json.loads(result.data)
         self.assertEqual(result.status_code, 404)
         self.assertEqual(data['success'], False)
 
-    #Create a Q
+    # Create a Q
     def test_create_question(self):
         result = self.client().post('/questions', json=self.question)
         data = json.loads(result.data)
         self.assertEqual(result.status_code, 200)
         self.assertEqual(data['success'], True)
 
-    #Get all Q in a Category
+    # Get all Q in a Category
     def test_getAll_questions_in_category(self):
         result = self.client().get('/categories/1/questions')
         data = json.loads(result.data)
@@ -93,28 +96,36 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertTrue(data['questions'])
 
-    #Get Qes from a not found category!
+    # Get Qes from a not found category!
     def get_questions_in_category_404(self):
         result = self.client().get('/categories/1234/questions')
         data = json.loads(result.data)
         self.assertEqual(result.status_code, 404)
         self.assertEqual(data['success'], False)
 
-    #get Q play mode.
+    # get Q play mode.
     def test_get_quiz_question(self):
-        #post req with the required params.
-        result = self.client().post('/quizzes', json={'quiz_category': {'type': 'Science', 'id': '1'}, 'previous_questions': []})
+        # post req with the required params.
+        result = self.client().post('/quizzes',
+                                    json={'quiz_category':
+                                          {'type': 'Science', 'id': '1'},
+                                          'previous_questions': []})
         data = json.loads(result.data)
         self.assertEqual(result.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['question'])
 
     def test_quiz_question_404(self):
-        #post req with the required params.
-        result = self.client().post('/quizzes', json={'quiz_category': {'type': 'none', 'id': '8888'}, 'previous_questions': []})
+        # post req with the required params.
+        result = self.client().post('/quizzes',
+                                    json={'quiz_category':
+                                          {'type': 'none', 'id': '8888'},
+                                          'previous_questions': []})
         data = json.loads(result.data)
         self.assertEqual(result.status_code, 404)
-        self.assertEqual(data['success'], False)      
+        self.assertEqual(data['success'], False)
+
+
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
